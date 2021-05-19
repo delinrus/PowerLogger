975458a60e7e61aec77e7e32fc2442f0ec903c4f
Maksim Levin <levmaxmih88@mail.ru>
2021-05-19 20:12:39 +0700

    Added COM-Port library and implemented simple reading

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,25 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.InputStream;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 
 
 public class App extends Application {
+    private static SerialPort serialPort;
+    private static Label helloWorldLabel;
+
     public static void main(String[] args) {
         Application.launch();
     }
 
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("PowerLogger");
         primaryStage.setWidth(300);
         primaryStage.setHeight(200);
@@ -21,11 +27,42 @@ public class App extends Application {
         Image image = new Image(getClass().getResourceAsStream("/thunderbolt.png"));
         primaryStage.getIcons().add(image);
 
-        Label helloWorldLabel = new Label("Hello world!");
+        helloWorldLabel = new Label("Hello world!");
         helloWorldLabel.setAlignment(Pos.CENTER);
         Scene primaryScene = new Scene(helloWorldLabel);
         primaryStage.setScene(primaryScene);
 
         primaryStage.show();
+        testPort();
+    }
+
+    public static void testPort() {
+        serialPort = new SerialPort("COM1");
+        try {
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+            serialPort.writeString("V00\r\n");
+        } catch (SerialPortException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static class PortReader implements SerialPortEventListener {
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if (event.isRXCHAR() && event.getEventValue() > 0) {
+                try {
+                    String data = serialPort.readString(event.getEventValue());
+                    Platform.runLater(() -> helloWorldLabel.setText(data));
+                    serialPort.writeString("V00\r\n");
+                } catch (SerialPortException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
