9f1de83b2fdc838d83211f7c9ab8b6845b6f2808
Maksim Levin <levmaxmih88@mail.ru>
2021-05-19 21:10:15 +0700

    Implemented PowerMeterReader

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,12 @@
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
-import jssc.SerialPortException;
 
 
 public class App extends Application {
-    private static SerialPort serialPort;
-    private static Label helloWorldLabel;
 
     public static void main(String[] args) {
         Application.launch();
@@ -27,42 +20,13 @@ public class App extends Application {
         Image image = new Image(getClass().getResourceAsStream("/thunderbolt.png"));
         primaryStage.getIcons().add(image);
 
-        helloWorldLabel = new Label("Hello world!");
+        Label helloWorldLabel = new Label("Hello world!");
         helloWorldLabel.setAlignment(Pos.CENTER);
         Scene primaryScene = new Scene(helloWorldLabel);
         primaryStage.setScene(primaryScene);
 
         primaryStage.show();
-        testPort();
-    }
 
-    public static void testPort() {
-        serialPort = new SerialPort("COM1");
-        try {
-            serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_9600,
-                    SerialPort.DATABITS_8,
-                    SerialPort.STOPBITS_1,
-                    SerialPort.PARITY_NONE);
-            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
-            serialPort.writeString("V00\r\n");
-        } catch (SerialPortException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private static class PortReader implements SerialPortEventListener {
-        @Override
-        public void serialEvent(SerialPortEvent event) {
-            if (event.isRXCHAR() && event.getEventValue() > 0) {
-                try {
-                    String data = serialPort.readString(event.getEventValue());
-                    Platform.runLater(() -> helloWorldLabel.setText(data));
-                    serialPort.writeString("V00\r\n");
-                } catch (SerialPortException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
+        new PowerMeterReader(power -> System.out.println("Power = " + power + " W"));
     }
 }
---
+++ src/main/java/PowerMeterReader.java
@@ -0,0 +1,68 @@
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class PowerMeterReader implements AutoCloseable {
+    private SerialPort serialPort;
+    private final Consumer<Double> callback;
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    public PowerMeterReader(Consumer<Double> callback) {
+        this.callback = callback;
+        openPort();
+        startCommunication();
+    }
+
+    @Override
+    public void close() throws Exception {
+        executorService.shutdownNow();
+        serialPort.closePort();
+    }
+
+    private void openPort() {
+        serialPort = new SerialPort("COM1");
+        try {
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+        } catch (SerialPortException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void sendQuery() {
+        try {
+            serialPort.writeString("V02\r\n");  // Sending command to read Watt https://www.gwinstek.com/en-GB/products/downloadSeriesDownNew/11635/987
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startCommunication() {
+        executorService.scheduleAtFixedRate(this::sendQuery, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private class PortReader implements SerialPortEventListener {
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if (event.isRXCHAR() && event.getEventValue() > 0) {
+                try {
+                    String data = serialPort.readString(event.getEventValue());
+                    callback.accept(Double.valueOf(data));
+                } catch (SerialPortException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
