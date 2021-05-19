f1890885b6693b5584c94fd8bef1efd4842f681f
Maksim Levin <levmaxmih88@mail.ru>
2021-05-19 17:16:54 +0700

    Added javafx to maven

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class App extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("PowerLogger");
+        stage.show();
     }
 }
