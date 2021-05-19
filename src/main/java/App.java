0d272c85d741247f113b65352a6bbbdb53d41471
Maksim Levin <levmaxmih88@mail.ru>
2021-05-19 18:40:15 +0700

    Added Icon, label, title

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,31 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
+
 
 public class App extends Application {
     public static void main(String[] args) {
         Application.launch();
     }
 
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("PowerLogger");
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("PowerLogger");
+        primaryStage.setWidth(300);
+        primaryStage.setHeight(200);
+
+        Image image = new Image(getClass().getResourceAsStream("/thunderbolt.png"));
+        primaryStage.getIcons().add(image);
+
+        Label helloWorldLabel = new Label("Hello world!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Scene primaryScene = new Scene(helloWorldLabel);
+        primaryStage.setScene(primaryScene);
+
+        primaryStage.show();
     }
 }
