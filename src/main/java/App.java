81156e29922a7882257d7a6931f74095f92fa6c3
Maksim Levin <levmaxmih88@mail.ru>
2021-05-20 16:13:34 +0700

    Added FreeChart example

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,17 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.fx.ChartViewer;
+import org.jfree.data.statistics.HistogramDataset;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -22,8 +27,8 @@ public class App extends Application {
 
     private void initStage(Stage primaryStage) {
         primaryStage.setTitle("PowerLogger");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(200);
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(400);
 
         Image image = new Image(getClass().getResourceAsStream("/thunderbolt.png"));
         primaryStage.getIcons().add(image);
@@ -37,9 +42,15 @@ public class App extends Application {
             System.out.println(newValue);
         });
 
-        FlowPane root = new FlowPane();
+        StackPane root = new StackPane();
+        root.setPadding(new Insets(10, 10, 10, 10));
         root.getChildren().add(datePicker);
-        root.setPadding(new Insets(10));
+
+        ChartViewer viewer = new ChartViewer(createChart());
+        root.getChildren().add(viewer);
+        StackPane.setMargin(viewer, new Insets(40, 0, 0, 0));
+        StackPane.setAlignment(datePicker, Pos.TOP_LEFT);
+
 
         Scene primaryScene = new Scene(root);
         primaryStage.setScene(primaryScene);
@@ -69,6 +80,25 @@ public class App extends Application {
             }
         };
         return dayCellFactory;
+    }
+
+    public static JFreeChart createChart() {
+
+        double[] values = {95, 49, 14, 59, 50, 66, 47, 40, 1, 67,
+                12, 58, 28, 63, 14, 9, 31, 17, 94, 71,
+                49, 64, 73, 97, 15, 63, 10, 12, 31, 62,
+                93, 49, 74, 90, 59, 14, 15, 88, 26, 57,
+                77, 44, 58, 91, 10, 67, 57, 19, 88, 84
+        };
+
+
+        HistogramDataset dataset = new HistogramDataset();
+        dataset.addSeries("key", values, 20);
+
+        JFreeChart histogram = ChartFactory.createHistogram("JFreeChart Histogram",
+                "y values", "x values", dataset);
+
+        return histogram;
     }
 
     @Override
