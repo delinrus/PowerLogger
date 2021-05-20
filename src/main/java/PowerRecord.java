b6d75c657183a0a15ab6a0885f4c3090e52cdc71
Maksim Levin <levmaxmih88@mail.ru>
2021-05-20 12:09:35 +0700

    Replaced Record with POJO

--- src/main/java/PowerRecord.java
+++ src/main/java/PowerRecord.java
@@ -1,5 +1,43 @@
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public record PowerRecord(double power, LocalDateTime time) implements Serializable {
+public class PowerRecord implements Serializable {
+    public double power;
+    public LocalDateTime time;
+
+    public PowerRecord(double power, LocalDateTime time) {
+        this.power = power;
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PowerRecord record = (PowerRecord) o;
+        return Double.compare(record.power, power) == 0 &&
+                Objects.equals(time, record.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(power, time);
+    }
+
+    @Override
+    public String toString() {
+        return "PowerRecord{" +
+                "power=" + power +
+                ", time=" + time +
+                '}';
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
 }
