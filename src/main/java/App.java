3a1a545d898e5faa4b244418d300ec2c566d6681
Maksim Levin <levmaxmih88@mail.ru>
2021-05-20 14:48:39 +0700

    Added DatePicker

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,15 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 
 public class App extends Application {
@@ -22,12 +28,47 @@ public class App extends Application {
         Image image = new Image(getClass().getResourceAsStream("/thunderbolt.png"));
         primaryStage.getIcons().add(image);
 
-        Label helloWorldLabel = new Label("Hello world!");
-        helloWorldLabel.setAlignment(Pos.CENTER);
-        Scene primaryScene = new Scene(helloWorldLabel);
+        DatePicker datePicker = new DatePicker();
+        datePicker.setValue(LocalDate.now());
+        datePicker.setShowWeekNumbers(true);
+        datePicker.setDayCellFactory(getDayCellFactory());
+
+        datePicker.valueProperty().addListener((ov, oldValue, newValue) -> {
+            System.out.println(newValue);
+        });
+
+        FlowPane root = new FlowPane();
+        root.getChildren().add(datePicker);
+        root.setPadding(new Insets(10));
+
+        Scene primaryScene = new Scene(root);
         primaryStage.setScene(primaryScene);
 
         primaryStage.show();
+    }
+
+    private Callback<DatePicker, DateCell> getDayCellFactory() {
+
+        final Callback<DatePicker, DateCell> dayCellFactory = new Callback<>() {
+            @Override
+            public DateCell call(DatePicker param) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        //Disable Monday, Tuesday, Wednesday
+                        if (item.getDayOfWeek() == DayOfWeek.MONDAY
+                                || item.getDayOfWeek() == DayOfWeek.TUESDAY
+                                || item.getDayOfWeek() == DayOfWeek.WEDNESDAY) {
+                            setDisable(true);
+                            setStyle("-fx-background-color: #ffc0cb;");
+                        }
+                    }
+                };
+            }
+        };
+        return dayCellFactory;
     }
 
     @Override
